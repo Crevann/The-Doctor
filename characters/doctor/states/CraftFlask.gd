bbe36cad97eca9851af0570db3726d8d04e6f3a9
Stefano Canavesi <ultratails2001@gmail.com>
2022-12-12 20:23:01 +0100

    Potion crafting particles

--- characters/doctor/states/CraftFlask.gd
+++ characters/doctor/states/CraftFlask.gd
@@ -3,5 +3,7 @@ extends CharacterState
 #Refer to the enum inside Doctor.gd
 export var potion: int
 
-func _frame_10():
+func _frame_4():
+	$"%PotionParticles".start_emitting()
+	host.play_sound("PotionCrafted")
 	host.push_potion(potion)
