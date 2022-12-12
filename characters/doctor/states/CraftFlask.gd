20226f74637f5b178268cab56073ebc7a4d6601f
Stefano Canavesi <ultratails2001@gmail.com>
2022-12-12 20:52:22 +0100

    Craft particle colors

--- characters/doctor/states/CraftFlask.gd
+++ characters/doctor/states/CraftFlask.gd
@@ -1,9 +1,13 @@
 extends CharacterState
 
 #Refer to the enum inside Doctor.gd
+export var _c_Potion = 0
 export var potion: int
+export(Color) var particle_colors
+
 
 func _frame_4():
+	$"%PotionParticles".get_node("Potion").color = particle_colors
 	$"%PotionParticles".start_emitting()
 	host.play_sound("PotionCrafted")
 	host.push_potion(potion)
